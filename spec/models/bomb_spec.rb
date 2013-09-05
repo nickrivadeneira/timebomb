6483a844811c292e4532d0402ee037841ec5b988
Nick Rivadeneira <nicholas.rivadeneira@gmail.com>
2013-09-05 16:38:17 -0400

    Test Bomb#send_request instance method.

--- spec/models/bomb_spec.rb
+++ spec/models/bomb_spec.rb
@@ -4,4 +4,25 @@ describe Bomb do
   it 'has fields' do
     expect(described_class).to have_fields :url, :request_params, :timestamp
   end
+
+  context 'instance' do
+    let(:bomb_params){
+      {
+        url:            'http://example.com',
+        request_params: {foo: 1, bar: 2}.to_json,
+        timestamp:      Time.now.to_i
+      }
+    }
+    let(:bomb){Bomb.create(bomb_params)}
+
+    describe 'method to send HTTP request' do
+      it 'sends a POST request' do
+        request = bomb.send_request
+
+        expect(request.request.http_method).to eq Net::HTTP::Post
+        expect(request.request.path.host).to eq URI.parse(bomb.url).host
+        expect(request.request.options[:body]).to eq bomb.request_params
+      end
+    end
+  end
 end
