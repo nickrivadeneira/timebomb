9503a321190aefcdfa7bc002a6a7dea60051a68a
Nick Rivadeneira <nicholas.rivadeneira@gmail.com>
2013-09-05 16:55:22 -0400

    Test timed scope.

--- spec/models/bomb_spec.rb
+++ spec/models/bomb_spec.rb
@@ -1,16 +1,34 @@
 require 'spec_helper'
 
 describe Bomb do
+  let(:timestamp){Time.now.to_i}
   let(:params){
     {
       url:            'http://example.com',
       request_params: {foo: 1, bar: 2}.to_json,
-      timestamp:      Time.now.to_i
+      timestamp:      timestamp
     }
   }
 
   it 'has fields' do
     expect(described_class).to have_fields :url, :request_params, :timestamp
+  end
+
+  describe 'timed scope' do
+    context 'with 2 out-of-scope records and 1 in-scope record' do
+      before do
+        interval_minutes = 60
+        offsets = [-60 * interval_minutes * 2, 0, 60 * interval_minutes * 2]
+        offsets.each do |offset|
+          described_class.create(timestamp: timestamp + offset)
+        end
+      end
+
+      it 'returns the in-scope record' do
+        expect(described_class.count).to eq 3
+        expect(described_class.timed(timestamp).count).to eq 1
+      end
+    end
   end
 
   describe 'method to send HTTP request' do
