68b33af63dc35dc29070a881c35f88dec77c8f98
Nick Rivadeneira <nicholas.rivadeneira@gmail.com>
2013-09-10 17:21:26 -0400

    Use Bomb#send_request instance method instead of class method. Instance method includes Model#destroy to discard sent messages.

--- jobs.rb
+++ jobs.rb
@@ -5,10 +5,10 @@ include Stalker
 job 'queue.requests' do |args|
   bombs = Bomb.timed args['timestamp']
   bombs.each do |bomb|
-    enqueue 'send.request', url: bomb.url, request_params: bomb.request_params
+    enqueue 'send.request', id: bomb._id
   end
 end
 
 job 'send.request' do |args|
-  Bomb.send_request args['url'], args['request_params']
+  Bomb.find(args['id']).send_request
 end
